196bd2397069aea3b2709c30675ec25935469002
Laszlo Szakacs <cocinerox@gmail.com>
2014-12-17 14:25:23 +0100

    support OS X hopefully

--- src/retro.c
+++ src/retro.c
@@ -1,7 +1,7 @@
 // Non-blocking keyboard input with 10 ms delay (get_ch)
 // Reference: http://stackoverflow.com/a/6665815 and http://stackoverflow.com/a/13129698
 
-#ifdef __unix__
+#if defined(__unix__) || defined(__APPLE__)
 
   #include <stdio.h>
   #include <termios.h>
